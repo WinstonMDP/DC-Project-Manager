5df4c19a5d7ee8ca8c63f8d0f510f00da497faa2
WinstonMDP <winstonmdp@gmail.com>
2023-03-28 09:28:47 +0300

    something

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM archlinux
-RUN pacman --noconfirm -Syu
-RUN pacman --noconfirm -S git docker
-RUN pacman --noconfirm -S clang rustup && rustup default stable
-RUN systemctl start docker.service && git clone https://github.com/studsovet/action_ddb && cd action_ddb && cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,29 @@
-use std::env;
-use std::process::Stdio;
-use std::thread;
+use execute::shell;
+use std::{fs, thread};
 
-fn fn_to_thread(init_command_variable_value: &str) -> impl FnOnce() + '_ {
-    move || {
-        env::set_var("COMMAND", init_command_variable_value);
-        let before_output = action_ddb::output("src/before.action");
-        println!("{before_output}");
-        // TODO: send before_output to mongodb
-        let after_output = action_ddb::output("src/after.action");
-        assert_eq!("", after_output);
-    }
-}
-
-fn fn_to_thread_2(cmd: &str) -> impl FnOnce() + '_ {
+fn fn_to_thread(filepath: &str) -> impl FnOnce() + '_ {
     move || {
         println!(
             "{}",
             String::from_utf8(
-                execute::shell(format!("{}{}", "docker run --rm archlinux bash {}", cmd))
-                    .stderr(Stdio::inherit())
-                    .output()
-                    .unwrap()
-                    .stdout,
+                shell(format!(
+                    "echo 161281 | su -c 'docker run --rm archlinux bash -c \"{}\"' MDPDockerUser",
+                    fs::read_to_string(filepath).unwrap()
+                ))
+                .output()
+                .unwrap()
+                .stdout,
             )
-            .expect("hey")
+            .unwrap()
         )
     }
 }
 
 fn main() {
-    let handle_1 = thread::spawn(fn_to_thread_2("echo 'Hey'"));
-    let handle_2 = thread::spawn(fn_to_thread_2("echo 'Ya'"));
-    let handle_3 = thread::spawn(fn_to_thread_2("echo 'No'"));
+    let handle_1 = thread::spawn(fn_to_thread("src/before.action"));
+    let handle_2 = thread::spawn(fn_to_thread("src/before.action"));
+    //let handle_3 = thread::spawn(fn_to_thread("echo 'No'"));
     handle_1.join().unwrap();
     handle_2.join().unwrap();
-    handle_3.join().unwrap();
-    let mut a = Vec::new();
-    a.push(1);
+    //handle_3.join().unwrap();
 }
