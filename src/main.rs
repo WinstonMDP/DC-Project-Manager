2b2edeffa116aa99c3971df259f670beea09334b
WinstonMDP <winstonmdp@gmail.com>
2023-03-29 07:05:07 +0300

    something

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
 use execute::shell;
+use std::io::Error;
+use std::process::Output;
 use std::{fs, thread};
 
-fn fn_to_thread(filepath: &str) -> impl FnOnce() + '_ {
+enum Action {
+    Before,
+    After,
+}
+
+fn action_closure<'a>(action: Action) -> impl FnOnce() -> Result<Output, Error> + 'a {
+    let filepath = "src/".to_owned()
+        + match action {
+            Action::Before => "before.action",
+            Action::After => "after.action",
+        };
     move || {
-        println!(
-            "{}",
-            String::from_utf8(
-                shell(format!(
-                    "echo 161281 | su -c 'docker run --rm archlinux bash -c \"{}\"' MDPDockerUser",
-                    fs::read_to_string(filepath).unwrap()
-                ))
-                .output()
-                .unwrap()
-                .stdout,
-            )
-            .unwrap()
-        )
+        shell(format!(
+            "echo 161281 | su -c 'docker run --rm archlinux bash -c \"{}\"' MDPDockerUser",
+            fs::read_to_string(filepath).unwrap()
+        ))
+        .output()
     }
 }
 
 fn main() {
-    let handle_1 = thread::spawn(fn_to_thread("src/before.action"));
-    let handle_2 = thread::spawn(fn_to_thread("src/before.action"));
-    //let handle_3 = thread::spawn(fn_to_thread("echo 'No'"));
-    handle_1.join().unwrap();
-    handle_2.join().unwrap();
-    //handle_3.join().unwrap();
+    let handle_1 = thread::spawn(action_closure(Action::Before));
+    let handle_2 = thread::spawn(action_closure(Action::Before));
+    let handle_3 = thread::spawn(action_closure(Action::After));
+    handle_1.join().unwrap().unwrap();
+    handle_2.join().unwrap().unwrap();
+    handle_3.join().unwrap().unwrap();
 }
